f0aa69dd976795d716b1edb3e66d628c4db09739
Paul Cuzner <pcuzner@redhat.com>
2021-08-02 15:11:08 +1200

    build:Update container to include ceph-common
    ceph-common installed from alpine community, to
    provide ceph cli integration. Version is 16.2.5
    
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- buildah/buildcmon.sh
+++ buildah/buildcmon.sh
@@ -6,6 +6,10 @@
 # setting up the alias
 # alias cmon="podman run --interactive --tty --net=host -e TERM -e CEPH_URL=http://192.168.122.92:9283/metrics -e PROMETHEUS_URL=http://192.168.122.92:9095 --entrypoint='/cmon.py' localhost/cmon:testing "
 # this allows you to use the container with parameters like this cmon -a or cmon -a -i
+#
+# with the ceph cli integration
+# alias cmon='podman run --interactive --tty --net=host -v /home/paul/etc_ceph:/etc/ceph:ro,z -e TERM -e CEPH_URL=http://192.168.122.92:9283/metrics -e PROMETHEUS_URL=http://192.168.122.92:9095 --entrypoint='\''/cmon.py'\'' localhost/cmon:devel'
+#
 if [ ! -z "$1" ]; then
   TAG=$1
 else
@@ -23,6 +27,7 @@ buildah run $container apk add py3-yaml
 buildah run $container apk add py3-requests
 buildah run $container apk add py3-setuptools
 buildah run $container apk add py3-urwid --repository http://dl-cdn.alpinelinux.org/alpine/edge/community/
+buildah run $container apk add ceph-common --repository http://dl-cdn.alpinelinux.org/alpine/edge/community/
 buildah run $container apk add py3-humanize --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/
 
 buildah run $container mkdir -p /cmon
